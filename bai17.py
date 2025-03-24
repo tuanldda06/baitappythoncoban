5c2d3f556771cf111f25aa28ab1fd57320ab41e3
tuanldda06 <leducanhtuan006@gmail.com>
2025-03-24 22:30:10 +0700

    Update bai17.py

--- bai17.py
+++ bai17.py
@@ -1,3 +1,6 @@
 #Nhập và kiểm tra ba số a, b, c có là cạnh của một tam giác không?
 a,b,c = map(float,input('Nhap so do cua ba canh:').split())
-if a+b>c and b+c>
+if a+b>c and b+c>a and a+c>b:
+  print('{} {} {} la ba canh cua mot tam giac'.format(a,b,c))
+else :
+  print('{} {} {} khong phai la ba canh cua mot tam giac'.format(a,b,c))
