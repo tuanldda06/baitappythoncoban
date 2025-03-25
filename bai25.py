180e55cf6c4a95417439730ab9518541c888cdb9
tuanldda06 <leducanhtuan006@gmail.com>
2025-03-25 15:27:07 +0700

    Update bai25.py

--- bai25.py
+++ bai25.py
@@ -1,1 +1,9 @@
+#Hiển thị 10 số tự nhiên đầu tiên ra màn hình trên từng dòng
+#Viết chương trình hiển thị 10 số tự nhiên đầu tiên ra màn hình, mỗi số nằm trên một dòng. Sau khi thực hiện xong in chữ “Done!”
+for i in range(10) :
+  print(i)
+else :
+  print('Done!')
 
+#for i in ... → Đây là cú pháp của vòng lặp for, trong đó i là biến đếm (iterator), mỗi lần lặp nó sẽ nhận một giá trị mới.
+#range(10) → Hàm range(10) tạo ra một dãy số từ 0 đến 9 (10 số, bắt đầu từ 0 và kết thúc trước 10).
