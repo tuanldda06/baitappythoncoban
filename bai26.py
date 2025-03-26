9d28f85a3a9af0671f6a710bbe951d23f595ebd0
tuanldda06 <leducanhtuan006@gmail.com>
2025-03-26 14:46:01 +0700

    Update bai26.py

--- bai26.py
+++ bai26.py
@@ -1,1 +1,8 @@
 #Tính tổng các số nguyên trong khoảng a đến b. (Vòng lặp for)
+#Input : Gồm hai dòng :Dòng đầu tiên chứa số nguyên a , dòng thứ hai chứa số nguyên b
+#Output :Gồm một dòng duy nhất hiển thị tổng các số trong khoảng từ a đến b
+#Lưu ý: 
+#Nếu a > b thì xuất thông báo: So thu nhat lon hon so thu hai!
+#Nếu input nằm ngoài định dạng đầu vào thì xuất thông báo: Dinh dang dau vao khong hop le!
+a = int(input('Nhap so dau tien : '))
+b = int(input('Nhap so thu 
