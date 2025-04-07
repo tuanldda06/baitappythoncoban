358138bb46e79c8c7a303d9220ae51e2363c8967
tuanlda78202 <tuanleducanh78202@gmail.com>
2025-04-07 09:58:02 +0000

    Them bai 8

--- baitap/8_bai8.py
+++ baitap/8_bai8.py
@@ -1,6 +1,5 @@
 #Nhập & tính tổng dãy số nguyên bất kỳ cách nhau bởi khoảng trắng.
-a =input('Nhap day so can tinh la:')
-b = list(map(int, a.split()))
-def tuan(danhsach):
-    return sum(danhsach)
-print('Tong la:',tuan(b))    
+a = input('Nhap day so can tinh la:')   
+b = list(map(int,a.split()))
+tongdayso = sum(b)
+print(b)
