3afbbf111d03478391f0997368bf2bfe38abf5ca
tuanlda78202 <tuanleducanh78202@gmail.com>
2025-04-14 08:31:11 +0000

    add

--- baitap/25_bai25.py
+++ baitap/25_bai25.py
@@ -1,6 +1,6 @@
 #Viết chương trình hiển thị 10 số tự nhiên đầu tiên ra màn hình, mỗi số nằm trên một dòng. Sau khi thực hiện xong in chữ “Done!”
 #Cau truc lap for
-for i in range(10):
+for  in range(10):
    #Cac lenh trong vong for duoc thuc hien nhieu lan
    print(i)
 #Menh de else duoc thuc hien khi vong lap ket thuc binh thuong, khong bi break
