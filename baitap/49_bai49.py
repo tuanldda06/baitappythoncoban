173dbbec80573e2f401506cc3f332c1a8cf1b923
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-10 10:32:48 +0000

    Thembai49

---
+++ baitap/49_bai49.py
@@ -0,0 +1,14 @@
+#Viết hàm tính n giai thừa (n!). Với tham số là số tự nhiên n.
+def giaithua(n):
+    if s == 1 :
+        return 1
+    else :
+        return n * giaithua(n-1)
+try :
+    n = int(input())
+    if n < 0 :
+        print('Vui long nhap so tu nhien')
+    else :
+        print(giaithua(n))
+except :
+    print('Dinh dang dau vao khong hop le')
