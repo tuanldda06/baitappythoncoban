5ed5755c3f48615519718490b4e7fc77203e5ddc
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-10 16:35:03 +0000

    Add solution for problem 53

--- baitap/53_bai53.py
+++ baitap/53_bai53.py
@@ -1,1 +1,6 @@
-print('Tuannnnnn1n111')
+#Viết hàm hiển thị độ dài chuỗi và hiển thị chuỗi ra màn hình. Với tham số là chuỗi s.
+def xuly(s) :
+    print('Do dai chuoi la' , len(s))
+    print('Chuoi do la', s)
+s = input()
+xuly(s)
