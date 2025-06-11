fb05366a3c068b12c20e0d8b04635cd215f727d8
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-11 18:19:03 +0000

    Thembai58

--- baitap/58_bai58.py
+++ baitap/58_bai58.py
@@ -1,15 +1,13 @@
-#Trả về chuỗi kết quả sau khi xóa các ký tự chẵn nếu chuỗi s có độ dài chẵn. (Tương tự xóa các ký tự lẻ với chuỗi lẻ).
+#Viết hàm truyền vào tham số là chuỗi s. 
+#Trả về chuỗi kết quả bằng cách thay thế đuôi "ing" bằng đuôi "ly" nếu chuỗi s kết thúc bằng "ing", nếu không thì thêm đuôi "ing" vào chuỗi s.
 def xuly(s):
-    chuoi = ""
-    dodaichuoi = len(s)
-    for i in range(dodaichuoi):
-        if i % 2 != dodaichuoi % 2 :
-            chuoi += s[i]           
-    return chuoi
-s = input()
+    if s[-3:] !='ing' and len(s)<3 :
+        return s + 'ing'
+    else :
+        return s[:-3] +'ly'
+s =input()
 print(xuly(s))
-#Vòng lặp kiểm tra i % 2 != dodaichuoi % 2:
-#Khi chuỗi chẵn (dodaichuoi % 2 == 0) → giữ i % 2 != 0 → giữ các vị trí lẻ
-#Khi chuỗi lẻ (dodaichuoi % 2 == 1) → giữ i % 2 != 1 → giữ các vị trí chẵn
-#Tức là: giữ lại các ký tự không cùng "tính chẵn lẻ" với độ dài chuỗi.
+#Trong s[:-3], Python lấy từ vị trí 0 đến end-1 = -3-1 = -4 (theo chỉ số âm), tức là bỏ 3 ký tự cuối (-3, -2, -1). Điều này khớp với yêu cầu bỏ "ing" trong hàm.
+#Còn s[-3:] Số -3 chỉ vị trí bắt đầu, tính ngược từ cuối chuỗi (vị trí cuối là -1, kế cuối là -2, thứ ba từ cuối là -3) còn Dấu : nghĩa là lấy từ vị trí -3 đến hết chuỗi.
+#Trong Python, cú pháp lát cắt s[start:end] được sử dụng để lấy một phần của chuỗi (hoặc danh sách, tuple, v.v.) từ vị trí start đến vị trí end-1.
 
