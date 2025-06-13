b81287f0d0cec0724b6f15562699ab59c2783feb
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-13 14:41:52 +0000

    Thembai70

---
+++ baitap/70_bai70.py
@@ -0,0 +1,16 @@
+#Viết hàm với tham số truyền vào là chuỗi s. Trả về tổng và trung bình cộng của các từ là số tự nhiên trong chuỗi s.
+def xuly(s):
+    tong = 0
+    dem = 0
+    for c in s :
+        if c.isdigit :
+            dem +=1
+            tong +=int(c)
+        if dem == 0 :
+            return 0 , 0
+        tb = tong / dem
+        return tong , tb
+s = input()
+tong,trungbinh = xuly(s)
+print(tong)
+print(trungbinh)
