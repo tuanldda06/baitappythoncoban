ac7800a7f869e85e9d7960e29b842aa1d19847d8
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-13 17:03:49 +0000

    Thembai72

---
+++ baitap/72_bai72.py
@@ -0,0 +1,36 @@
+#Viết hàm với tham số truyền vào là chuỗi s. Trả về số lượng từ vừa chứa ký tự, vừa chứa chữ số trong chuỗi s.
+#Cách 1
+def xuly(s):
+    dem = 0 
+    for tu in s.split() :# nếu không có split() thì nó sẽ duyệt cả khoảng trắng, ở đây yêu cầu xét từng kí tự
+       cochu = False
+       coso = False
+       for c in tu :
+           if c.isdigit() :
+               coso = True
+           if c.isalpha() :
+               cochu = True
+       if coso and cochu :
+           dem +=1
+    return dem    
+s = input()
+print(xuly(s))
+#Cách 2
+def xuly(s) :
+    dem = 0
+    for tu in s.split() :# nếu không có split() thì nó sẽ duyệt cả khoảng trắng, ở đây yêu cầu xét từng kí tự
+       
+       coso = 0
+       cochu = 0
+       for c in tu :
+           if c.isdigit() :
+               coso +=1
+           if c.isalpha() :
+               cochu +=1
+       if coso >=1 and cochu>=1 :
+           dem +=1
+    return dem
+s = input()
+print(xuly(s))
+           
+
