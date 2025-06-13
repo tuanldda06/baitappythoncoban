4d09e96259d7bbd6f45c0e3c8526a1eb60b87bd6
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-13 11:12:08 +0000

    Thembai69

---
+++ baitap/69_bai69.py
@@ -0,0 +1,23 @@
+#Viết hàm truyền vào tham số là chuỗi s. Hiển thị số lần xuất hiện của các ký tự trong chuỗi s.
+def xuly(s):
+    for i in s :
+        solan = s.count(i)
+        print("'{}' :{}".format(i , solan))
+s = input()
+xuly(s)
+#Hàm của bạn hoạt động đúng về mặt đếm số lần xuất hiện ký tự. Tuy nhiên, nó sẽ in trùng nhiều lần nếu ký tự xuất hiện nhiều hơn 1 lần.
+#Cách làm tối ưu nhất
+def xuly(s) :
+    chuoi = ""
+    for i in s :
+        if i not in chuoi :
+            chuoi +=i
+    for i in chuoi: 
+        print(" '{}': {} ; ".format(i , s.count(i)),end='') # 
+s = input()
+xuly(s)
+#end='' → in xong không xuống dòng.
+
+end=' ' → in xong xuống dòng bằng 1 dấu cách.
+
+end='\n' (mặc định) → in xong xuống dòng mới.
