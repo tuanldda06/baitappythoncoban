d518480e4f7f4ba65e6c8b5f545394cb414c61de
tuanldda06 <leducanhtuan006@gmail.com>
2025-06-29 03:44:00 +0000

    Thembai66

--- baitap/66_bai66.py
+++ baitap/66_bai66.py
@@ -1,2 +1,11 @@
-#Viết hàm trả về từ có độ dài lớn nhất trong chuỗi s. Với tham số truyền vào là chuỗi s. Nếu có nhiều từ có độ dài bằng nhau thì trả về từ có thứ tự nhỏ hơn (sắp xếp theo bảng chữ cái).
-
+#Viết hàm trả về từ có độ dài lớn nhất trong chuỗi s. Với tham số truyền vào là chuỗi s.
+# Nếu có nhiều từ có độ dài bằng nhau thì trả về từ có thứ tự nhỏ hơn (sắp xếp theo bảng chữ cái).
+def xuly(s) :
+    tudainhat = ''
+    danhsach = s.split()
+    for tu in danhsach :
+        if (len(tu) > len(tudainhat)) or ((len(tu)==len(tudainhat)) and (tu<tudainhat)):
+            tudainhat = tu
+    return tudainhat
+s = input()
+print(xuly(s))
