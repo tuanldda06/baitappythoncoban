ecc6a3acbf264921e1b861b9cef171065872a51f
tuanldda06 <leducanhtuan006@gmail.com>
2025-07-02 19:00:56 +0000

    Thembai1

--- thuvien/NumPy/01_bai1.py
+++ thuvien/NumPy/01_bai1.py
@@ -5,3 +5,13 @@
 #Tính tổng, trung bình, và giá trị lớn nhất của mảng 2D.
 import numpy as np
 arr_1d= np.arange(10)
+print('Mang 1D:' , arr_1d)
+arr_2d = np.arange(1,10).reshape(3,3)
+print('\nMang2D : \n',arr_2d)
+total = np.sum(arr_2d)
+mean = np.mean(arr_2d)
+max_val = np.max(arr_2d)
+print('\nTong:',total)
+print('Trung binh',mean)
+print('Gia tri lon nhat',max_val)
+
