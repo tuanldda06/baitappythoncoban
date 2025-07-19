9007571a7bc2761be9371280c6f2514a4aa10465
tuanldda06 <leducanhtuan006@gmail.com>
2025-07-19 23:46:08 +0700

    Create Runnerup.py

---
+++ hkr/Runnerup.py
@@ -0,0 +1,6 @@
+#Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score.
+# You are given n scores. Store them in a list and find the score of the runner-up.
+#Example : [5 6  6 3 3 7 7 ] --> second runner : 6
+danhsach = list(map(int , input().split())
+dsach = []
+for i in danhsach : 
