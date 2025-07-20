16936bb7a36b6074c9bb7cb49aa18ac7071a60dd
tuanldda06 <leducanhtuan006@gmail.com>
2025-07-20 15:34:09 +0700

    Thembai83

---
+++ baitap/83_bai83.py
@@ -0,0 +1,2 @@
+#Viết hàm với tham số truyền vào là một danh sách và số tự nhiên n. Trả về danh sách n phần tử bằng cách lặp lại danh sách được truyền vào.
+#VD: danhsach :'Kteam Kteam 2021 Chuc Mung Nam Moi ' và n = 3 --> Output :Kteam Kteam 2021
