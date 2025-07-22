c6197a966266129cd339af3d6f2d26dd4bc0da4a
tuanldda06 <leducanhtuan006@gmail.com>
2025-07-22 13:30:17 +0700

    Thembai84

--- baitap/84_bai84.py
+++ baitap/84_bai84.py
@@ -1,5 +1,8 @@
 #Viết hàm với tham số truyền vào là một danh sách và số nguyên dương n.
 #Trả về danh sách kết quả bằng cách chèn phần tử “Kteam” vào các vị trí chia hết cho n.
 def xuly(danhsach , n ) :
+  ketqua = []
+  for thutu in range(0,len(danhsach) , n ) :
+    ketqua.extend(
   
 
