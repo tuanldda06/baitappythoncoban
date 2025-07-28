8e131a699a0091f5f7709ee25b5ae65d69f39351
tuanldda06 <leducanhtuan006@gmail.com>
2025-07-29 00:35:14 +0700

    Update 02_bai2.py

--- thuvien/NumPy/02_bai2.py
+++ thuvien/NumPy/02_bai2.py
@@ -1,3 +1,8 @@
 #Bài toán vector hóa , yêu cầu :
 # Tạo hai mảng 1D kích thước 5 , chứa só ngẫu nhiên từ 1 đến 10
 # Tính tổng tích và hiệu của hai mảng không dùng đến vòng lặp
+import numpy as np
+np.random.seed(42)
+#đảm bảo kết quả được tái lập có nghĩa là mỗi lần bạn chạy mã với cùng giá trị seed() thì các số ngấu nhiên được taọ ra bởi hàm np.random.randiant() sẽ luôn giống nhau về giá trị và thứ tự
+arr1 = np.random.randint(1,11, size = 5)
+arr2 = np.random.randint
