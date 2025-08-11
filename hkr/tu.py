a0a6d721a9d459672f5e7651b918c5d2b74db97f
tuanldda06 <leducanhtuan006@gmail.com>
2025-08-11 21:33:23 +0700

    Delete hkr directory

--- hkr/tu.py
+++
@@ -1,7 +0,0 @@
-#i < n, print i^2
-#The provided code stub reads an integer, n, from STDIN. For all non-negative integers
-#Example :n = 3
-#The list of non-negative integers that are less than n = 3 is (0, 1, 2). Print the square of each number on a separate
-n = int(input())
-for i in range(0,n) :
-    print(i*i)
