d5050fdb7bf4393ca2cc31e4d5f076ee0b54b332
tuanldda06 <leducanhtuan006@gmail.com>
2025-08-20 14:46:42 +0700

    Thembai95

---
+++ baitap/95_bai95.py
@@ -0,0 +1,6 @@
+#Viết chương trình khởi tạo hai tuple() bất kỳ bằng hai cách: Sử dụng cặp dấu ngoặc tròn và sử dụng Constructor tuple(). Hiển thị hai tuple đó ra màn hình.
+tupleA = 'Kteam' , 70 , True
+print(tupleA)
+
+tupleB = [('Kteam',70 , False)]
+print(tupleB)
